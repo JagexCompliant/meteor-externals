fb634d69192185723bd828fa4ef257fa6cc56c1e
Kaballist <Darwinmackay@gmail.com>
2023-01-03 11:44:01 -0800

    modify gradle files bump versions

--- meteor-external/build.gradle.kts
+++ meteor-external/build.gradle.kts
@@ -1,6 +1,6 @@
 
 
-version = "1.0.3"
+version = "1.0.4"
 
 project.extra["PluginName"] = "Example Plugin"
 project.extra["PluginDescription"] = "This is an example plugin"
@@ -9,16 +9,14 @@ repositories{
 }
 
 tasks {
-
     jar {
         manifest {
             attributes(mapOf(
+                "Main-Class" to "example.MainClass",
                 "Plugin-Version" to project.version,
                 "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
                 "Plugin-Description" to project.extra["PluginDescription"],
-            )
-         )
-
+            ))
         }
     }
 }
--- meteor-external2/build.gradle.kts
+++ meteor-external2/build.gradle.kts
@@ -1,5 +1,5 @@
 
-version = "1.0.3"
+version = "1.0.4"
 
 project.extra["PluginName"] = "Example Second"
 project.extra["PluginDescription"] = "This is an example plugin"
@@ -8,17 +8,14 @@ project.extra["PluginDescription"] = "This is an example plugin"
 
 
 tasks {
-
     jar {
         manifest {
-
             attributes(mapOf(
+                "Main-Class" to "example.MainClass",
                 "Plugin-Version" to project.version,
                 "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
                 "Plugin-Description" to project.extra["PluginDescription"],
-            )
-            )
-
+            ))
         }
     }
 }
