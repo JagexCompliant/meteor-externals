c51d7ad8ff02aa8337af78066e9ca0a27f797d98
Kaballist <Darwinmackay@gmail.com>
2023-01-03 05:16:41 -0800

    bump version

--- meteor-external/build.gradle.kts
+++ meteor-external/build.gradle.kts
@@ -1,6 +1,6 @@
 
 
-version = "1.0.0"
+version = "1.0.1"
 
 project.extra["PluginName"] = "Example Plugin"
 project.extra["PluginDescription"] = "This is an example plugin"
--- meteor-external2/build.gradle.kts
+++ meteor-external2/build.gradle.kts
@@ -1,5 +1,5 @@
 
-version = "1.0.0"
+version = "1.0.1"
 
 project.extra["PluginName"] = "Example Second"
 project.extra["PluginDescription"] = "This is an example plugin"
